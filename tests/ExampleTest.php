1c37678daeb71c0bfde63d028dfa421219f5eefa
Carlos Aguilar <carlosaguilarnet@gmail.com>
2015-09-25 00:00:25 -0500

    Minor changes in tests

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -13,7 +13,8 @@ class ExampleTest extends TestCase
      */
     public function testBasicExample()
     {
-        $this->visit('/')
-             ->see('Laravel 5');
+        $response = $this->call('GET', '/auth/login');
+
+        $this->assertEquals(200, $response->status());
     }
 }
